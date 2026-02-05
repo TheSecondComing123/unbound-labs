287f4e56105b2f421ed737132a424ef7e2bdfd10
TheSecondComing123 <jiuprogrammer@gmail.com>
2026-02-05 17:03:33 -0500

    Migrate static pages to EJS, add themes & deps
    Replace static HTML pages with EJS templates and update server to render views. Added views/layout.ejs and views/pages/*.ejs, removed public/pages/*.html, and updated src/server.js to use the new templating. Implemented multi-theme support: expanded CSS variables (quartz, slate, midnight), adjusted layout (features grid columns), and updated theme toggle logic in public/js/main.js to cycle themes and update icons/ARIA. Added runtime dependencies (ejs, helmet) in package.json.

--- src/server.js
+++ src/server.js
@@ -1,41 +1,55 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const helmet = require('helmet');
 
 const app = express();
 const PORT = 9090;
 
+app.use(helmet());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-     res.sendFile(path.join(__dirname, '../public/pages/home.html'));
+     const pageContent = fs.readFileSync(path.join(__dirname, '../views/pages/home.ejs'), 'utf8');
+     res.render('layout', { title: 'Home', currentPage: 'home', body: pageContent });
 });
 
 app.get('/home', (req, res) => {
-     res.sendFile(path.join(__dirname, '../public/pages/home.html'));
+     res.redirect('/');
 });
 
 app.get('/projects', (req, res) => {
-     res.sendFile(path.join(__dirname, '../public/pages/projects.html'));
+     const pageContent = fs.readFileSync(path.join(__dirname, '../views/pages/projects.ejs'), 'utf8');
+     res.render('layout', { title: 'Projects', currentPage: 'projects', body: pageContent });
 });
 
 app.get('/projects/unbound', (req, res) => {
-     res.sendFile(path.join(__dirname, '../public/pages/unbound.html'));
+     const pageContent = fs.readFileSync(path.join(__dirname, '../views/pages/unbound.ejs'), 'utf8');
+     res.render('layout', { title: 'unbound Browser', currentPage: 'projects', body: pageContent });
 });
 
 app.get('/projects/uncensored', (req, res) => {
-     res.sendFile(path.join(__dirname, '../public/pages/uncensored.html'));
+     const pageContent = fs.readFileSync(path.join(__dirname, '../views/pages/uncensored.ejs'), 'utf8');
+     res.render('layout', { title: 'uncensored Proxy', currentPage: 'projects', body: pageContent });
 });
 
 app.get('/about', (req, res) => {
-     res.sendFile(path.join(__dirname, '../public/pages/about.html'));
+     const pageContent = fs.readFileSync(path.join(__dirname, '../views/pages/about.ejs'), 'utf8');
+     res.render('layout', { title: 'About', currentPage: 'about', body: pageContent });
 });
 
 app.get('/contact', (req, res) => {
-     res.sendFile(path.join(__dirname, '../public/pages/contact.html'));
+     const pageContent = fs.readFileSync(path.join(__dirname, '../views/pages/contact.ejs'), 'utf8');
+     res.render('layout', { title: 'Contact', currentPage: 'contact', body: pageContent });
 });
 
 app.use((req, res) => {
-     res.status(404).sendFile(path.join(__dirname, '../public/pages/404.html'));
+     const pageContent = fs.readFileSync(path.join(__dirname, '../views/pages/404.ejs'), 'utf8');
+     res.status(404).render('layout', { title: '404', currentPage: '', body: pageContent });
 });
 
 app.listen(PORT, () => {
